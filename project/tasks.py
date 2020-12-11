ba932f3992e12cc4c49e11e9ac83f75b53c26122
Min ho Kim <minho42@gmail.com>
2020-12-12 05:30:16 +1100

    add optional parameters

--- project/tasks.py
+++ project/tasks.py
@@ -7,13 +7,16 @@ from .twitter_saver import TwitterSaver
 
 
 @shared_task
-def save_info(project_id: int) -> None:
-    with ScreenshotSaver() as ss:
-        ss.save(project_id)
+def save_info(project_id: int, screenshot=True, avatar=True, bio=True) -> None:
+    if screenshot:
+        with ScreenshotSaver() as ss:
+            ss.save(project_id)
 
     ts = TwitterSaver()
-    ts.save_bio(project_id)
-    ts.save_profile_image(project_id)
+    if bio:
+        ts.save_bio(project_id)
+    if avatar:
+        ts.save_profile_image(project_id)
 
     # TODO call generate_json here for automation
     # TODO check permission or catch permission exception
@@ -21,9 +24,9 @@ def save_info(project_id: int) -> None:
 
 
 @shared_task
-def save_info_for_all() -> None:
+def save_info_for_all(screenshot=True, avatar=True, bio=True) -> None:
     Project = apps.get_model("project", "Project")
     total = Project.objects.count()
     for p in Project.objects.all():
         # print(f"{index+1}/{total}")
-        save_info(p.id)
+        save_info(p.id, screenshot=True, avatar=True, bio=True)
